b274ca74fb476861c0ed43b5c3ebaddd130dca3b
TiannaLaird <tiannalairdx123@gmail.com>
2023-12-03 18:18:33 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+  let thankYouMessages = [];
+
+  for (let i = 0; i < cards.length; i++) {
+    thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+
+  return thankYouMessages;
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
